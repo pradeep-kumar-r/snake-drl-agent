a463658687b11d46753ab932d67d06ddf71514ab
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-06-15 04:51:44 +0400

    Updated food

--- src/game/food.py
+++ src/game/food.py
@@ -1,57 +1,55 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Optional
 import random
-from src.config import config
 
 
 class Food(ABC):
-    def __init__(self, board_dim: int):
+    def __init__(self, board_dim: int) -> None:
         self.board_dim: int = board_dim
         self.position: Optional[Tuple[int, int]] = None
+        self.active: bool = False
     
     @abstractmethod
-    def place_food(self, snake_body: List[Tuple[int, int]]):
+    def place_food(self, snake_body: List[Tuple[int, int]]) -> None:
         pass
 
     @abstractmethod
-    def is_eaten(self, snake_head: Tuple[int, int]):
+    def is_eaten(self, snake_head: Tuple[int, int]) -> bool:
         pass
 
     @abstractmethod
-    def update(self):
+    def update(self) -> None:
         pass
 
 
 class SimpleFood(Food):
-    def __init__(self, 
-                 board_dim: int=config.get_game_config().BOARD_DIM):
+    def __init__(self, board_dim: int):
         super().__init__(board_dim)
         self.remaining_steps = 0
         
-    def place_food(self, snake_body: List[Tuple[int, int]]):
+    def place_food(self, snake_body: List[Tuple[int, int]]) -> None:
         while True:
             pos = (random.randint(-1*self.board_dim//2, self.board_dim//2), random.randint(-1*self.board_dim//2, self.board_dim//2))
             if pos not in snake_body:
                 self.position = pos
+                self.active = True
                 break
 
-    def is_eaten(self, snake_head: Tuple[int, int]):
-        return snake_head == self.position
+    def is_eaten(self, snake_head: Tuple[int, int]) -> bool:
+        return self.active and snake_head == self.position
 
-    def update(self):
+    def update(self) -> None:
         pass
 
 
 class SuperFood(Food):
-    def __init__(self, 
-                 board_dim: int=config.get_game_config().BOARD_DIM,
-                 lifetime: int=config.get_training_config().FOOD.SUPERFOOD_LIFETIME):
+    def __init__(self, board_dim: int, lifetime: int):
         super().__init__(board_dim)
         self.lifetime = lifetime
         self.remaining_steps = 0
         self.active = False
 
-    def place_food(self, snake_body: List[Tuple[int, int]]):
+    def place_food(self, snake_body: List[Tuple[int, int]]) -> None:
         while True:
             pos = (random.randint(-1*self.board_dim//2, self.board_dim//2), random.randint(-1*self.board_dim//2, self.board_dim//2))
             if pos not in snake_body:
@@ -61,11 +59,11 @@ class SuperFood(Food):
                 break
         self.active = False
 
-    def is_eaten(self, snake_head: Tuple[int, int]):
+    def is_eaten(self, snake_head: Tuple[int, int]) -> bool:
         return self.active and snake_head == self.position
 
-    def update(self):
+    def update(self) -> None:
         if self.active:
             self.remaining_steps -= 1
-            if self.remaining_steps <= 0:
+            if self.remaining_steps < 0:
                 self.active = False
