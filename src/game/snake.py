9549de7ce85b74b316109443d28d5f38fba4766c
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-06-15 04:51:38 +0400

    Updated snake

--- src/game/snake.py
+++ src/game/snake.py
@@ -1,66 +1,63 @@
-from typing import Tuple
-from .direction import Direction
-from src.config import config
+from typing import Tuple, List
+from src.game.direction import Direction
+from src.utils.utils import add_tuples
 
 
 class Snake:
     def __init__(self, 
-                 game_config = config.get_game_config):
-        self.game_config = game_config
-        self.board_dim: int = self.game_config.BOARD_DIM
-        self.init_pos: Tuple[int, int] = self.game_config.SNAKE.SNAKE_INIT_POS
-        self.init_length: int = self.game_config.SNAKE.SNAKE_INIT_LENGTH
-        self.init_direction: Direction = self.game_config.SNAKE.SNAKE_INIT_DIRECTION
+                 board_dim: int,
+                 init_pos: Tuple[int, int],
+                 init_length: int,
+                 init_direction: Direction):
+        self.board_dim = board_dim
+        self.init_pos = init_pos
+        self.init_length = init_length
+        self.direction = self.init_direction = init_direction
+        self.should_grow: bool = False
+        self.alive: bool = True
         x, y = self.init_pos
-        self.body = [(x - i, y) for i in range(self.init_length)]
-        self.direction = self.init_direction
-        self.grow_pending = 0
-        self.alive = True
-
-    def set_direction(self, new_direction: Direction):
+        self.body: List[Tuple[int, int]] = [(x - i, y) for i in range(self.init_length)]
+        
+    def set_direction(self, new_direction: Direction) -> None:
+        # Check for 180 degree turns and modify direction
         if not Direction.is_opposite(self.direction, new_direction):
             self.direction = new_direction
 
-    def move(self):
-        left_wall_x = - 1 * self.board_dim//2
-        right_wall_x = self.board_dim//2
-        top_wall_y = self.board_dim//2
-        bottom_wall_y = - 1 * self.board_dim//2
+    def move(self) -> None:
+        boundary = self.board_dim//2
+        left_bound_x = bottom_bound_y = -1*boundary
+        right_bound_x = top_bound_y = boundary
         if not self.alive:
             return
-        head_x, head_y = self.body[0]
-        dx, dy = self.direction.value
-        new_head = (head_x + dx, head_y + dy)
-        if new_head[0] < left_wall_x:
-            new_head = (right_wall_x, new_head[1])
-        elif new_head[0] > right_wall_x:
-            new_head = (left_wall_x, new_head[1])
-        elif new_head[1] < bottom_wall_y:
-            new_head = (new_head[0], top_wall_y)
-        elif new_head[1] > top_wall_y:
-            new_head = (new_head[0], bottom_wall_y)
+        head = self.body[0]
+        new_head = add_tuples(head, self.direction.value)
+        if new_head[0] < left_bound_x:
+            new_head = (right_bound_x, new_head[1])
+        elif new_head[0] > right_bound_x:
+            new_head = (left_bound_x, new_head[1])
+        elif new_head[1] < bottom_bound_y:
+            new_head = (new_head[0], top_bound_y)
+        elif new_head[1] > top_bound_y:
+            new_head = (new_head[0], bottom_bound_y)
         self.body.insert(0, new_head)
-        if self.grow_pending > 0:
-            self.grow_pending -= 1
+        if self.should_grow:
+            self.should_grow = False
         else:
             self.body.pop()
 
-    def grow(self, amount: int=1):
-        self.grow_pending += amount
-
-    def check_collision(self):
+    def check_collision(self) -> bool:
         head = self.body[0]
         return head in self.body[1:]
 
-    def get_head(self):
+    def get_head(self) -> Tuple[int, int]:
         return self.body[0]
 
-    def get_body(self):
+    def get_body(self) -> List[Tuple[int, int]]:
         return self.body
 
-    def get_direction(self):
+    def get_direction(self) -> Direction:
         return self.direction
 
-    def kill(self):
+    def kill(self) -> None:
         self.alive = False
         
