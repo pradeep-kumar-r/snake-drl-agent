6279da4fa86508453d7fe10886b62c9c523e2f3d
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-06-15 04:51:58 +0400

    Updated game

--- src/game/game.py
+++ src/game/game.py
@@ -1,5 +1,5 @@
 import random
-from typing import Optional, Literal, Tuple
+from typing import Optional, Literal, Tuple, Dict
 from src.game.direction import Direction
 from src.game.snake import Snake
 from src.game.food import Food, SimpleFood, SuperFood
@@ -7,47 +7,61 @@ from src.config import config
 
 
 class Game:
-    def __init__(self, configuration=config):
-        self.config = configuration
-        self.game_config = configuration.get_game_config()
+    def __init__(self, game_config=config.get_game_config()):
+        self.game_config = game_config
         self.snake: Optional[Snake] = None
-        self.food: Optional[Food] = None
+        self.is_food_active: bool = False
+        self.current_food: Optional[Food] = None
         self.steps_elapsed: int = 0
         self.food_count: int = 0
         self.score: int = 0
-        self.game_over: bool = False
-        self.is_food_active: bool = False
+        self.is_game_over: bool = False
+        self.end_state: Optional[Dict[str, int]] = None
         self.reset()
 
-    def reset(self):
-        self.snake = Snake(game_config=self.game_config)
-        self.food: Optional[Food] = None
+    def reset(self) -> None:
+        self.snake = Snake(board_dim=self.game_config.BOARD_DIM,
+                           init_pos=self.game_config.SNAKE.INIT_POS,
+                           init_length=self.game_config.SNAKE.INIT_LENGTH,
+                           init_direction=self.game_config.SNAKE.INIT_DIRECTION)
+        self._generate_or_update_food()
+        self.is_food_active: bool = False
+        self.current_food: Optional[Food] = None
         self.steps_elapsed: int = 0
         self.food_count: int = 0
         self.score: int = 0
-        self.game_over: bool = False
-        self.is_food_active: bool = False
+        self.is_game_over: bool = False
         
-    def generate_food(self):
-        if self.is_food_active:
-            return
-        if random.random() <= self.game_config.FOOD.SUPERFOOD_PROBABILITY:
-            self.food = SuperFood()
-        else:
-            self.food = SimpleFood()
-        snake_body = self.snake.get_body()
-        self.food.place_food(snake_body)
-        self.is_food_active = True
-        
-    def _is_food_eaten(self):
+    def _is_food_eaten(self) -> bool:
         if not self.is_food_active:
             return False
-        return self.food.is_eaten(self.snake.get_head())
+        return self.current_food.is_eaten(self.snake.get_head())
     
-    def is_game_over(self):
-        return self.game_over
+    def _generate_or_update_food(self) -> None:
+        # Start generating food only after the first 3 steps in a new game
+        if self.steps_elapsed < 3:
+            return
+        
+        if not self.current_food.active or not self.is_food_active:
+            if random.random() <= self.game_config.FOOD.SUPERFOOD_PROBABILITY and self.food_count > 0:
+                self.current_food = SuperFood(board_dim=self.game_config.BOARD_DIM,
+                                    lifetime=self.game_config.FOOD.SUPERFOOD_LIFETIME)
+            else:
+                self.current_food = SimpleFood(board_dim=self.game_config.BOARD_DIM)
+            self.current_food.place_food(self.snake.get_body())
+            self.is_food_active = True        
+        else:
+            self.current_food.update()
+        
+    def _update_score(self) -> None:
+        if not self.is_food_active:
+            return
+        if isinstance(self.current_food, SimpleFood):
+            self.score += self.game_config.SCORE.EAT_FOOD
+        else: 
+            self.score += self.game_config.SCORE.XPLIER_EAT_SUPERFOOD*(1+self.current_food.remaining_steps)    
     
-    def step(self, action: Literal[0, 1, 2, 3, 4]) -> Tuple[float, bool, int, int]:
+    def step(self, action: Literal[0, 1, 2, 3, 4]=0) -> Tuple[float, bool, int, int]:
         """
         Actions:
             0: STILL (do nothing)
@@ -58,9 +72,13 @@ class Game:
         Returns:
             tuple: (reward, game_over, score, steps_elapsed)
         """
-        training_rewards = self.config.get_training_config().REWARDS
-        current_reward = training_rewards.MOVE
-
+        
+        self.steps_elapsed += 1
+        
+        # Step 1: Generate/update food if no food is active
+        self._generate_or_update_food()
+        
+        # Step 2: Update snake direction and move it
         action_to_direction_map = {
             1: Direction.RIGHT,
             2: Direction.DOWN,
@@ -70,54 +88,53 @@ class Game:
 
         if action in action_to_direction_map:
             new_direction = action_to_direction_map[action]
-            if not Direction.is_opposite(new_direction, self.snake.direction):
-                self.snake.set_direction(new_direction)
-        else:
-            current_reward += training_rewards.NOTHING
-        
+            self.snake.set_direction(new_direction)
+
         self.snake.move()
 
-        # Check if food is active - update food, if not generate food
-        if self.is_food_active:
-            if not self.food.update():
-                self.is_food_active = False
-                
-        if not self.is_food_active:
-            self.generate_food()
-        
-        # Check if the food is eaten after the snake moves
+        # Step 3: Check if food is eaten after snake moves, if so grow snake, update food count & score
         if self._is_food_eaten():
             self.food_count += 1
-            if isinstance(self.food, SimpleFood):
-                current_reward += training_rewards.EAT_FOOD
-                self.score += self.game_config.SCORE.EAT_FOOD
-            else: 
-                super_food_value = self.food.remaining_steps if hasattr(self.food, 'remaining_steps') else 1
-                current_reward += training_rewards.XPLIER_EAT_SUPERFOOD * super_food_value
-                self.score += self.game_config.SCORE.XPLIER_EAT_SUPERFOOD * super_food_value
-            
-            self.is_food_active = False 
-            self.generate_food()
-        
-        # Check for collision
+            self._update_score()
+            self.snake.should_grow = True
+                
+        # Step 4: Check for collision
         if self.snake.check_collision():
-            self.game_over = True
-            current_reward += training_rewards.GAME_OVER_REWARD
-            self.record_score()
-
-        self.steps_elapsed += 1
-        return current_reward, self.game_over, self.score, self.steps_elapsed
+            self.is_game_over = True
+            self.snake.kill()
+            self._record_end_state()
         
-    def record_score(self):
+        return (self.steps_elapsed, 
+                self.is_game_over, 
+                self.score,
+                self.snake.get_body(),
+                self.snake.get_direction(),
+                self.is_food_active,
+                self.current_food.position if self.is_food_active else None,
+                self.current_food.remaining_steps if self.is_food_active and isinstance(self.current_food, SuperFood) else None)
+        
+    def _record_end_state(self):
         if self.is_game_over:
-            score_file = self.config.data_config.GAME_DATA_FOLDER.joinpath("scores.txt")
-            with open(score_file, "a", encoding="utf-8") as f:
-                f.write(str(self.score) + "\n")
+            self.end_state = {
+                "steps_elapsed": self.steps_elapsed, 
+                "food_count": self.food_count,
+                "score": self.score,
+            }
+    
+    def record_score(self):
+        ...
+        # if self.is_game_over:
+        #     score_file = self.config.data_config.GAME_DATA_FOLDER.joinpath("scores.txt")
+        #     with open(score_file, "a", encoding="utf-8") as f:
+        #         f.write(str(self.score) + "\n")
             
-            high_score_file = self.config.data_config.GAME_DATA_FOLDER.joinpath("high_score.txt")
-            with open(high_score_file, "r", encoding="utf-8") as f:
-                high_score = int(f.read())
+        #     high_score_file = self.config.data_config.GAME_DATA_FOLDER.joinpath("high_score.txt")
+        #     with open(high_score_file, "r", encoding="utf-8") as f:
+        #         high_score = int(f.read())
             
-            if self.score > high_score:
-                with open(high_score_file, "w", encoding="utf-8") as f:
-                    f.write(str(self.score))
+        #     if self.score > high_score:
+        #         with open(high_score_file, "w", encoding="utf-8") as f:
+        #             f.write(str(self.score))
+                    
+    def display_ui(self):
+        ...
