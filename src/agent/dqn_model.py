27f8c8e1b2531c9e910b8e41a934a560230a4ef9
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-06-22 03:13:26 +0400

    DQN model update to take 3 input channels (color image)

--- src/agent/dqn_model.py
+++ src/agent/dqn_model.py
@@ -6,7 +6,7 @@ class ConvDQN(nn.Module):
     def __init__(self, num_classes: int):
         super().__init__()
         self.num_classes = num_classes
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
