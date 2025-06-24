819de1355242a84e7775bc5004f24fe087c15573
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-06-24 04:28:18 +0400

    Added colour enum

---
+++ src/game/colour.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Colour(Enum):
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    PURPLE = (128, 0, 128)
+    BLUE = (0, 0, 255)
+    LIGHT_GREY = (211, 211, 211)
